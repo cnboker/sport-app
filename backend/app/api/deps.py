9bf9171384c5532175433e1abc2add0d32bf69ab
cnboker <coobots@gmail.com>
2026-03-12 15:57:01 +0800

    create user

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,3 +1,5 @@
+from typing import AsyncGenerator
+
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine
@@ -7,7 +9,7 @@ DATABASE_URL = "postgresql+asyncpg://postgres:mysecret@localhost/mysport"
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 # 2. 依赖项：获取异步 Session
-async def get_session() -> AsyncSession:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     async with async_session() as session:
         yield session   
